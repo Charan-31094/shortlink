69870c87370b6e1127f651c0883269c77d93bb11
Charan <2100031094@kluniversity.in>
2023-07-06 22:18:44 +0530

    c

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const urlParams = new URLSearchParams(window.location.search);
-const uid = urlParams.get('id');
+value=window.location.pathname
+val=value.split("/")[2]
+id=btoa(val)
 
 function redirectToURL(k) {
-    window.location.href =k; // Replace with your desired URL
+    window.location.href ="https://"+k;
   }
 const db = new polybase.Polybase({
     defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
   })
   const col = db.collection("LinkData");
   async function listRecords () {
-    const records = await col.where("id", "==", "23456756").get();
+    const records = await col.where("id", "==", id).get();
     let s=records.data[0].data
    let k=JSON.stringify(records.data[0].data)
    console.log(k)
     document.getElementById("ch").innerHTML =` <div class="info">
     <p>Full URL: <strong>${s.full_url}</strong></p>
-    <p>Short Name: <strong>John Doe</strong></p>
-    <p>Email: <strong>johndoe@example.com</strong></p>
+    <p>Short Name: <strong>${s.name}</strong></p>
+    <p>Email: <strong>${s.email}</strong></p>
   </div>
   <button type="button" onclick="redirectToURL('${s.full_url}')" name="redirect">Redirect</button>`
   }
