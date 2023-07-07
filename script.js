277c1ca9d29ba7f028c323a4b7724cd7c8dd1a45
Charan <2100031094@kluniversity.in>
2023-07-07 13:44:51 +0530

    s

--- script.js
+++ script.js
@@ -24,15 +24,16 @@ const db = new polybase.Polybase({
    
     let s=records.data[0].data 
     redirectToURL(s.full_url) 
-   let k=JSON.stringify(records.data[0].data)
-   console.log(k)
+  //  let k=JSON.stringify(records.data[0].data)
+  //  console.log(k)
    
-    document.getElementById("ch").innerHTML =` <div class="info">
-    <p>Full URL: <strong>${s.full_url}</strong></p>
-    <p>Short Name: <strong>${s.name}</strong></p>
-    <p>Email: <strong>Hidden</strong></p>
-  </div>
-  <button type="button" onclick="redirectToURL('${s.full_url}')" name="redirect">Redirect</button>` }
+  //   document.getElementById("ch").innerHTML =` <div class="info">
+  //   <p>Full URL: <strong>${s.full_url}</strong></p>
+  //   <p>Short Name: <strong>${s.name}</strong></p>
+  //   <p>Email: <strong>Hidden</strong></p>
+  // </div>
+  // <button type="button" onclick="redirectToURL('${s.full_url}')" name="redirect">Redirect</button>` 
+    }
   catch(e){
     document.getElementById("ch").innerHTML =` <div class="info">
     <p>Full URL: <strong>Not Found</strong></p>
