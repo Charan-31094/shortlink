4679e5be9f0ee1ad5596f4ebf5c119ed085b36c9
Charan <2100031094@kluniversity.in>
2023-07-08 10:09:57 +0530

    update and form valid

--- gnew/script.js
+++ gnew/script.js
@@ -1,13 +1,26 @@
+
 const db = new polybase.Polybase({
-    defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
-  })
-  const col = db.collection("LinkData");
-  async function listRecords () {
-    const records = await col.get();
-    console.log(JSON.stringify(records));
-    document.getElementById("dd").innerHTML = JSON.stringify(records.data);
+  defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
+})
+const col = db.collection("LinkData");
+
+  function send (){
+    try{
+    a=document.getElementById("nam").value;
+    b= document.getElementById('full_url').value
+    c=document.getElementById('short_url').value
+    d=document.getElementById('email').value}
+    catch(e){
+      console.log(e)
+    }
+    createRecord(a,b,c,d)
+   console.log("sent")
+   return false;
   }
+
+  
   async function createRecord (name,full_url,short_url,email) {
+
     document.getElementById("hh").innerHTML=`<strong style="color:white">Checking Availability</strong>`
 
     console.log(name,full_url,short_url,email)
--- update/script.js
+++ update/script.js
@@ -1,93 +1,124 @@
-var html=`<div class="col-lg-5">
-<div class="contact-wrap w-100 p-md-5 p-4">
-<h3 class="mb-4">New Shorty</h3>
-<div id="form-message-warning" class="mb-4"></div>
-<div id="form-message-success" class="mb-4">
 
-</div>
-<form >
-<div class="row">
-<div class="col-md-12">
+const urlParams = new URLSearchParams(window.location.search);
+const uid = urlParams.get('shorty');
 
-</div>
-<div class="col-md-12">
-<div class="form-group">
-<input type="text" class="form-control" name="email" id="email" placeholder="Email" required>
-</div>
-</div>
-<div class="col-md-12">
-<div class="form-group">
-<input type="text" class="form-control" name="full_url" id="full_url" placeholder="Enter Full URL www.ex.com/rufbrbujrfbujrbv..........">
-</div>
-</div>
-<div class="col-md-12">
-<div class="form-group">
-    <input type="text" class="form-control" name="short_url" id="short_url" placeholder="/short_url"  >
-</div>
-<div class="form-group">
-    <input type="text" class="form-control" name="nam" id="nam" placeholder="NickName for short url">
-    </div>
-    <!-- <div class="form-group">
-        <input type="text" class="form-control" name="name" id="name"  placeholder="info about url">
-        </div> -->
-</div>
-<div class="col-md-12">
-<div class="form-group">
-<input type="button" value="Send Message" onclick="createRecord(nam.value,full_url.value,short_url.value,email.value)"  class="btn btn-primary">
-<div class="submitting"></div>
-</div>
-</div>
-</div>
-</form>
-</div>`
+console.log(uid)
+id=btoa(uid);
+var m;
 
-function redirectToURL(k) {
-    if(text.includes("https://"))
-    {
-      window.location.href =k;
-    }
-    else{
-      window.location.href ="https://"+k;}
-    }
+//copy
+function copy(a) {
+  navigator.clipboard.writeText(a);
+  var k=document.getElementById("but1")
+  k.innerHTML="Copied"
+}
+
+
+
+//connection to db
   const db = new polybase.Polybase({
       defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
     })
     const col = db.collection("LinkData");
+
+
+
+//update records
+async function updateRecord(full_url,name) {
+  try{
+    const recordData = await col
+        .record(id)
+        .call("updateLink", [full_url,name]);
+
+document.getElementById("hh").innerHTML=`<strong style="color:white">Shorty Updated Successfully</strong>&nbsp&nbsp<a href="/${uid}">Visit</a>&nbsp&nbsp<button type="button" id="but1" class="button" onClick="copy('https://link.nixer.ml/${uid}')">Copy</button>`
+
+}
+catch(err){
+    console.log(err)
+}
+}
+
+//find records
     async function findRecords () {
       try{
       const records = await col.where("id", "==", id).get();
      
-      let s=records.data[0].data  
-     let k=JSON.stringify(records.data[0].data)
-     console.log(k)
-      document.getElementById("ch").innerHTML =` <div class="col-lg-5">
+      let s=records.data[0].data
+      m=s.email  
+      document.getElementById("ch").innerHTML =`<section class="ftco-section img bg-hero" style="background-image: url(../files/images/back.webp);">
+      <div class="container">
+      <div class="row justify-content-center">
+      <div class="col-md-6 text-center mb-5">
+      <h2 class="heading-section">Update Short Link</h2>
+      </div>
+      </div>
+      <div class="row justify-content-center">
+      <div class="col-lg-11">
+      <div class="wrapper">
+      <div class="row no-gutters justify-content-between">
+      <div class="col-lg-6 d-flex align-items-stretch">
+      <div class="info-wrap w-100 p-5">
+      <!-- <h3 class="mb-4">Contact us</h3> -->
+      <div class="dbox w-100 d-flex align-items-start">
+      <div class="icon d-flex align-items-center justify-content-center">
+      <span class="fa fa-link"></span>
+      </div>
+      <div class="text pl-4">
+      <p><span>No.of links active</span> <p id="nu"></p></p>
+      </div>
+      </div>
+      
+      <div class="dbox w-100 d-flex align-items-start">
+      <div class="icon d-flex align-items-center justify-content-center">
+      </div>
+      
+      </div>
+      <div class="dbox w-100 d-flex align-items-start">
+      <div class="icon d-flex align-items-center justify-content-center">
+      <span class="fa fa-globe"></span>
+      </div>
+      <div class="text pl-4">
+      <p><span>Website</span> <a href="#">link.nixer.ml</a></p>
+      </div>
+      </div>
+      <div class="dbox w-100 d-flex align-items-start">
+          <div class="icon d-flex align-items-center justify-content-center">
+              <span class="fa fa-database"></span> 
+          </div>
+          <div class="text pl-4">
+              <p><span>Status</span> <a id="hh"></a></p>
+          </div>
+          </div>
+      </div>
+      </div>
+      <div class="col-lg-5">
       <div class="contact-wrap w-100 p-md-5 p-4">
-      <h3 class="mb-4">New Shorty</h3>
+      <h3 class="mb-4">Update Shorty</h3>
       <div id="form-message-warning" class="mb-4"></div>
       <div id="form-message-success" class="mb-4">
       
       </div>
-      <form >
+      <form  id="myForm"  onsubmit="return send()" >
       <div class="row">
       <div class="col-md-12">
       
       </div>
       <div class="col-md-12">
       <div class="form-group">
-      <input type="text" class="form-control" name="email" id="email" placeholder="Email" required>
+      <input type="text" class="form-control" name="email" id="email" placeholder="Enter Email to update" required>
       </div>
       </div>
       <div class="col-md-12">
       <div class="form-group">
-      <input type="text" class="form-control" name="full_url" value="${s.full_url}" id="full_url" placeholder="Enter Full URL www.ex.com/rufbrbujrfbujrbv..........">
+      <input type="text" class="form-control" name="full_url" id="full_url" value=${s.full_url} required placeholder="Enter Full URL www.ex.com/rufbrbujrfbujrbv..........">
       </div>
       </div>
       <div class="col-md-12">
       <div class="form-group">
-          <input type="text" class="form-control" name="short_url" id="short_url" value=${short_url} placeholder="/short_url"  >
+          <input type="text" class="form-control" name="short_url" id="short_url" value=${s.short_url} minlength="4" required disabled>
       </div>
       <div class="form-group">
-          <input type="text" class="form-control" name="nam" id="nam" value=${s.name} placeholder="NickName for short url">
+          <input type="text" class="form-control" name="nam" id="nam" value=${s.name} required placeholder="NickName for short url">
           </div>
           <!-- <div class="form-group">
               <input type="text" class="form-control" name="name" id="name"  placeholder="info about url">
@@ -95,20 +126,54 @@ function redirectToURL(k) {
       </div>
       <div class="col-md-12">
       <div class="form-group">
-      <input type="button" value="Update" onclick="createRecord(nam.value,full_url.value,short_url.value,email.value)"  class="btn btn-primary">
-      <div class="submitting"></div>
+      <input type="submit" value="Update url"  class="btn btn-primary">
+      <div class="submitting" ></div>
+      
+      
       </div>
       </div>
       </div>
       </form>
-      </div>` }
-    catch(e){
-      document.getElementById("ch").innerHTML =` <div class="info">
-      <p>Full URL: <strong>Not Found</strong></p>
-      <p>Short Name: <strong>Not Found</strong></p>
-      <p>Email: <strong>Not Found</strong></p>
       </div>
-      <button type="button" onclick="redirectToURL('link.nixer.ml/gnew')" name="redirect">Create a New Link</button>`
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </section>` }
+    catch(e){
+     console.log(e)
     }
     }
+
+
+    function send()
+    {
+      try{
+       
+        a=document.getElementById("nam").value;
+        b= document.getElementById('full_url').value
+        c=uid
+        d=document.getElementById('email').value
+        if(d == m)
+        {
+          updateRecord(b,a);
+        }
+        else{
+          document.getElementById("hh").innerHTML=`<p><strong style="color:palered">Email Doesn't match please enter the mail that have been entered while creating url</strong></p>`
+        }
+      }
+        catch(e){
+          console.log(e)
+        }
+        return false;
+    }
+
+
+
+ 
+
+
+
     findRecords ()
