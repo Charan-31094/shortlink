ce66ad3f20319c9758e9997cd167369f48524b9e
Charan <2100031094@kluniversity.in>
2023-07-08 15:20:11 +0530

    mail cookie and loader

--- aclinks/app.js
+++ aclinks/app.js
@@ -1,3 +1,45 @@
+//cookies
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  function checkCookie() {
+    let user = getCookie("email");
+    if (user != "") {
+      console.log(user)
+  k=document.getElementById("email")
+  k.value=user
+  geturl(user)
+    } 
+  }
+
+
+//click enter fun
+var input = document.getElementById("email");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("submit_email").click();
+  }
+});
+
 //db connection
 const db = new polybase.Polybase({
     defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
@@ -6,6 +48,9 @@ const col = db.collection("LinkData");
 
 //find records
 async function geturl(email) {
+    document.getElementById("submit_email").disabled = true;
+
+    document.getElementById("loader").innerHTML='<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
     try {
         const records = await col.where("email", "==", email).get();
         let s = records
@@ -13,7 +58,8 @@ async function geturl(email) {
         tabu(s.data)
         
     }
-    catch (e) { console.log(e) }
+    catch (e) { console.log(e);     document.getElementById("loader").innerHTML=`<p>No Url's found please verify your email`
+}
 }
 
 //populate table
@@ -41,6 +87,9 @@ function tabu(data)
         <td><a href="../update/?shorty=${d.data.short_url}">Update</a></td>
       </tr>`
     });
+    document.getElementById("loader").innerHTML=``
     document.getElementById("table_data").innerHTML=htm
+    document.getElementById("submit_email").disabled = false;
+
 }
 
