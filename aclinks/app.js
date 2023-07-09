d4564b08edc8d14c9a45d3b7d8c5ecd39926115f
Charan <2100031094@kluniversity.in>
2023-07-09 12:12:54 +0530

    copy

--- aclinks/app.js
+++ aclinks/app.js
@@ -1,39 +1,45 @@
 //cookies
-function setCookie(cname,cvalue,exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
-  function checkCookie() {
-    let user = getCookie("email");
-    if (user != "") {
-      console.log(user)
-  k=document.getElementById("email")
-  k.value=user
-  geturl(user)
-    } 
+  return "";
+}
+function checkCookie() {
+  let user = getCookie("email");
+  if (user != "") {
+    console.log(user)
+    k = document.getElementById("email")
+    k.value = user
+    geturl(user)
   }
+}
 
+
+function copy(a) {
+  navigator.clipboard.writeText("https://9url.tech/"+a);
+  var k = document.getElementById("cb" + a)
+  k.innerHTML = "Copied"
+}
 
 //click enter fun
 var input = document.getElementById("email");
-input.addEventListener("keypress", function(event) {
+input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     document.getElementById("submit_email").click();
@@ -42,18 +48,17 @@ input.addEventListener("keypress", function(event) {
 
 //db connection
 const db = new polybase.Polybase({
-    defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
+  defaultNamespace: "pk/0x26b7b24287218ea9427e03112e34fb29e6603d1421903bedf5b825296f2d4f32831d76d700f243fbb768d935937675735ca28568f8604b10a33ac636a81fd9d2/links",
 })
 const col = db.collection("LinkData");
 
-function geturl(email)
-{
-        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if (input.match(validRegex)) {
-          alert("Valid email address!");
-        } else { alert("Invalid email address!");}
-      
-      }
+function geturl(email) {
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (input.match(validRegex)) {
+    alert("Valid email address!");
+  } else { alert("Invalid email address!"); }
+
+}
 
 
 
@@ -61,18 +66,19 @@ function geturl(email)
 
 //find records
 async function geturl(email) {
-    document.getElementById("submit_email").disabled = true;
+  document.getElementById("submit_email").disabled = true;
 
-    document.getElementById("loader").innerHTML='<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
-    try {
-        const records = await col.where("email", "==", email).get();
-        let s = records
-        console.log(s)
-        tabu(s.data)
-        
-    }
-    catch (e) { console.log(e);     document.getElementById("loader").innerHTML=`<p>No Url's found please verify your email`
-}
+  document.getElementById("loader").innerHTML = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
+  try {
+    const records = await col.where("email", "==", email).get();
+    let s = records
+    console.log(s)
+    tabu(s.data)
+
+  }
+  catch (e) {
+    console.log(e); document.getElementById("loader").innerHTML = `<p>No Url's found please verify your email`
+  }
 }
 
 
@@ -86,10 +92,10 @@ async function deleteRecord(cid) {
       .call("deleteLink", []);
     console.log(recordData);
   }
-  catch (err) { console.log(err)}
+  catch (err) { console.log(err) }
 }
 
-function deleteCard(button,a) {
+function deleteCard(button, a) {
   var cardContainer = button.closest(".card");
   cardContainer.remove();
   deleteRecord(a)
@@ -98,20 +104,20 @@ function deleteCard(button,a) {
 
 function generateRandomImageLink() {
   var randomImages = [
-      "https://img.freepik.com/free-photo/cream-smooth-textured-paper-background_53876-101819.jpg?size=626&ext=jpg&ga=GA1.2.2080235410.1688872253&semt=ais",
-      "https://img.freepik.com/free-photo/white-texture_1160-788.jpg?size=626&ext=jpg&ga=GA1.2.2080235410.1688872253&semt=ais",
-      "https://img.freepik.com/free-photo/blue-cotton-fabric_53876-88939.jpg?size=626&ext=jpg&ga=GA1.2.2080235410.1688872253&semt=ais",
-      "https://img.freepik.com/free-photo/cardboard-wallpaper-template_1194-6785.jpg?size=626&ext=jpg&ga=GA1.1.2080235410.1688872253&semt=ais",
-      "https://img.freepik.com/free-photo/blue-smooth-wall-textured-background_53876-106133.jpg?size=626&ext=jpg",
-      "https://img.freepik.com/free-vector/grunge-watercolor-background-using-pastel-colours_1048-6530.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/free-vector/abstract-pink-watercolor-texture-design-background_1055-14111.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/free-vector/white-abstract-background-3d-paper-style_23-2148398981.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/free-vector/watercolor-stains-abstract-background_23-2149107181.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/premium-photo/colorful-watercolor-grunge-texture-background-soft-background_3248-586.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/free-photo/3d-geometrical-texture_23-2149168862.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/free-photo/white-splash-ink-blue-background_1409-1757.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/free-vector/watercolor-background_91008-2.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
-      "https://img.freepik.com/premium-vector/white-abstract-texture-background_745217-602.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais"
+    "https://img.freepik.com/free-photo/cream-smooth-textured-paper-background_53876-101819.jpg?size=626&ext=jpg&ga=GA1.2.2080235410.1688872253&semt=ais",
+    "https://img.freepik.com/free-photo/white-texture_1160-788.jpg?size=626&ext=jpg&ga=GA1.2.2080235410.1688872253&semt=ais",
+    "https://img.freepik.com/free-photo/blue-cotton-fabric_53876-88939.jpg?size=626&ext=jpg&ga=GA1.2.2080235410.1688872253&semt=ais",
+    "https://img.freepik.com/free-photo/cardboard-wallpaper-template_1194-6785.jpg?size=626&ext=jpg&ga=GA1.1.2080235410.1688872253&semt=ais",
+    "https://img.freepik.com/free-photo/blue-smooth-wall-textured-background_53876-106133.jpg?size=626&ext=jpg",
+    "https://img.freepik.com/free-vector/grunge-watercolor-background-using-pastel-colours_1048-6530.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/free-vector/abstract-pink-watercolor-texture-design-background_1055-14111.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/free-vector/white-abstract-background-3d-paper-style_23-2148398981.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/free-vector/watercolor-stains-abstract-background_23-2149107181.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/premium-photo/colorful-watercolor-grunge-texture-background-soft-background_3248-586.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/free-photo/3d-geometrical-texture_23-2149168862.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/free-photo/white-splash-ink-blue-background_1409-1757.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/free-vector/watercolor-background_91008-2.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais",
+    "https://img.freepik.com/premium-vector/white-abstract-texture-background_745217-602.jpg?size=626&ext=jpg&ga=GA1.1.290949994.1688879249&semt=ais"
   ];
 
   var randomIndex = Math.floor(Math.random() * randomImages.length);
@@ -137,49 +143,47 @@ function initializeCard(cardId, imageId, qrCodeId, imageSize, qrCodeLink) {
 
   generateQRCode(qrCodeLink, qrCodeId, imageSize);
 
-  card.addEventListener("mouseover", function() {
-      qrCode.style.display = "block";
+  card.addEventListener("mouseover", function () {
+    qrCode.style.display = "block";
   });
 
-  card.addEventListener("mouseout", function() {
-      qrCode.style.display = "none";
+  card.addEventListener("mouseout", function () {
+    qrCode.style.display = "none";
   });
 }
 //populate table
 
 
-function intialize(data)
-{
+function intialize(data) {
   data.forEach(d => {
-    s=d.data
-    initializeCard("c"+s.short_url, "ab"+s.short_url, "qr"+s.short_url, 100, "https://9url.tech/"+s.short_url);
+    s = d.data
+    initializeCard("c" + s.short_url, "ab" + s.short_url, "qr" + s.short_url, 100, "https://9url.tech/" + s.short_url);
   });
   var searchInput = document.getElementById("searchInput");
-var cardContainers = document.getElementsByClassName("card");
+  var cardContainers = document.getElementsByClassName("card");
 
-searchInput.addEventListener("input", function() {
+  searchInput.addEventListener("input", function () {
     var searchValue = searchInput.value.toLowerCase();
 
-    Array.from(cardContainers).forEach(function(cardContainer) {
-        
-        var cardTitle = cardContainer.getElementsByTagName("h1")[0].innerText.toLowerCase();
+    Array.from(cardContainers).forEach(function (cardContainer) {
 
-        if (cardTitle.includes(searchValue)) {
-            cardContainer.style.display = "block";
-        } else {
-            cardContainer.style.display = "none";
-        }
+      var cardTitle = cardContainer.getElementsByTagName("h1")[0].innerText.toLowerCase();
+
+      if (cardTitle.includes(searchValue)) {
+        cardContainer.style.display = "block";
+      } else {
+        cardContainer.style.display = "none";
+      }
     });
-});
-  document.getElementById("loader").innerHTML=``
+  });
+  document.getElementById("loader").innerHTML = ``
   document.getElementById("submit_email").disabled = false;
 }
 
 
-function tabu(data)
-{
-    var n=0
-    var htm=`<div class="container">
+function tabu(data) {
+  var n = 0
+  var htm = `<div class="container">
     <div class="search-bar">
         <input type="text" id="searchInput" placeholder="Search...">
     </div>
@@ -188,11 +192,11 @@ function tabu(data)
 
 <div class="container">
 <div class="card-container">`
-    console.log(data.length)
-    data.forEach(d => {
-        n=n+1
-        s=d.data
-        htm=htm+`
+  console.log(data.length)
+  data.forEach(d => {
+    n = n + 1
+    s = d.data
+    htm = htm + `
         <div class="card" id="c${s.short_url}">
             <div class="qr-code">
                 <img id="qr${s.short_url}" src="https" alt="QR Code">
@@ -202,15 +206,16 @@ function tabu(data)
             <h3>No of Clicks : ${s.clicks}</h3>
             <p>${s.full_url}</p>
             <div class="button-container">
-                <a href="./update/?shorty=${s.short_url}" target="_blank">Update</a>
+                <a href="../update/?shorty=${s.short_url}" target="_blank">Update</a>
+                <button class="copy-button" id="cb${s.short_url}" onclick="copy('${s.short_url}')">Copy</button>
                 <button class="delete-button" onclick="deleteCard(this,'${s.id}')">Delete</button>
             </div>
         </div>`
-    });
-    htm=htm+` </div>
+  });
+  htm = htm + ` </div>
     </div>`
-    document.getElementById("cont").innerHTML=htm
-    intialize(data)
+  document.getElementById("cont").innerHTML = htm
+  intialize(data)
 
 }
 
